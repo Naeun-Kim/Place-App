b3967af7e2416fe200019f31bf88006dffcf313b
naeun kim <naeunkim@naeunui-MacBookAir.local>
2021-06-21 16:46:35 +0900

    add update post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch, useSelector } from 'react-redux';
+import { createPost, updatePost } from '../../actions/posts';
+
 import useStyles from './styles';
-import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/posts';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import { StyledButton, CustomButton } from '../UI/Button';
@@ -25,32 +26,45 @@ const theme = createMuiTheme({
   },
 });
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
         message: '',
         tags: '',
         selectedFile: ''
-    })
+    });
+    const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
     const classes = useStyles();
     const dispatch = useDispatch();
 
+    useEffect(() => {
+      if (post) setPostData(post);
+    }, [post]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(createPost(postData));
-    }
+
+        if (currentId === 0) {
+          dispatch(createPost(postData));
+          clear();
+        } else {
+          dispatch(updatePost(currentId, postData));
+          clear();
+        }
+    };
 
     const clear = () => {
-
-    }
+      setCurrentId(0);
+      setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    };
 
     return (
         <Paper className={classes.paper}>
             <form autocomplete="off"
                 noValidate className={`${classes.root} ${classes.form}`}
                 onSubmit={handleSubmit}>
-                {/* <Typography variant="h6">Creating a place</Typography> */}
+                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Place'}</Typography>
                 <TextField className={classes.inputItem} name="creator" variant="outlined" label="Creator" fullWidth
                 value={postData.creator} 
                 onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
@@ -62,7 +76,7 @@ const Form = () => {
                 onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
                 <TextField className={classes.inputItem} name="tags" variant="outlined" label="tags" fullWidth
                 value={postData.tags} 
-                onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+                onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
                 <div className={classes.fileInput}>
                     <FileBase
                         type="file"
