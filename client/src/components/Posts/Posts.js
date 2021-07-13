0222a49382c61d72ccf8d0fb18e6888703af9afe
naeun kim <naeunkim@naeunui-MacBookAir.local>
2021-07-13 12:34:32 +0900

    sorting posts by created date

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Grid, CircularProgress } from '@material-ui/core';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
-import Post from './Post/Post';
-import useStyles from './styles';
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const sortByDate = (a, b) => {
+  return new Date(b.createdAt) - new Date(a.createdAt);
+};
 
 const Posts = ({ setCurrentId }) => {
-    const posts = useSelector((state) => state.posts);
-    const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
 
-    console.log(posts);
-    return (
-        !posts.length ? <CircularProgress /> : (
-            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
-                    <Grid key={post.id} item xs={12} sm={6} md={6}>
-                        <Post post={post} setCurrentId={setCurrentId} />
-                    </Grid>
-                ))}
-            </Grid>
-        )
-    );
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.sort(sortByDate).map((post) => (
+        <Grid key={post.id} item xs={12} sm={6} md={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Posts;
